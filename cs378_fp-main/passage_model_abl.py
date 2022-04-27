d7f582d1c2a8dfb0fb2622d8d8c389108b285b92
Raymond <raymond.hong22@gmail.com>
2022-04-27 11:24:09 -0500

    passaage model ablation

---
+++ cs378_fp-main/passage_model_abl.py
@@ -0,0 +1,77 @@
+import json
+import random
+ 
+# Opening JSON file
+f = open('../squad_data/example_set.json')
+ 
+# returns JSON object as
+# a dictionary
+data = json.load(f)
+total = 0
+word_counter = dict()
+for i in data['data']:
+    p = (i['context'])
+    for w in p.split():
+        if w not in word_counter:
+            word_counter[w]=0
+        word_counter[w]+=1
+        total+=1
+print(word_counter)
+
+def get_random():
+    r = random.randrange(total)
+    for key in word_counter:
+        r = r - word_counter[key]
+        if (r < 0):
+            return key
+    return "NAN"
+
+
+#Assigns gibberish using unigram model
+for i in data['data']:
+    p = i['context']
+    txt = (p.split())
+
+    ans=(i["answers"])
+    ans_st = ans["answer_start"]
+    ans_txt = ans["text"]
+    
+    ans_map = dict()
+    for j in range(len(ans_st)):
+        if ans_st[j] not in ans_map:
+            ans_map[ans_st[j]] = list()
+        ans_map[ans_st[j]].append(ans_txt[j])
+        ans_map[ans_st[j]].sort(reverse=True)
+    print(ans_map)
+    
+    randoms = dict()
+    for k in ans_map:
+        n = random.randrange(len(txt))
+        randoms[k] = (n)
+
+    res = ""
+    new_ans_st = list()
+    new_ans_text = list()
+    for idx in range(len(txt)):
+        temp = (get_random())
+        added=False
+
+        for k in ans_map:
+            st = len(res)
+            if (idx == randoms[k]):
+                for w in ans_map[k]:
+                    new_ans_st.append(st)
+                    new_ans_text.append(w)
+                res += ans_map[k][0] + " "
+                added=True
+
+        if (not added):
+            res+=(temp) + " "
+    i["context"] = res
+    ans["answer_start"] = new_ans_st
+    ans["text"] = new_ans_text
+print(data)
+
+with open('test.json', 'w') as g:
+    json.dump(data, g)
+f.close()
